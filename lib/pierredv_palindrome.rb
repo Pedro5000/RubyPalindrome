a561726a4436ef72e87b3425a655a5e53665e090
Pedro5000 <pierre.de.vreyer@gmail.com>
2020-05-16 17:16:23 +0800

    Initialize repository

---
+++ lib/pierredv_palindrome.rb
@@ -0,0 +1,6 @@
+require "pierredv_palindrome/version"
+
+module PierredvPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pierredv_palindrome/version.rb
@@ -0,0 +1,3 @@
+module PierredvPalindrome
+  VERSION = "0.1.0"
+end
